ff50a788ac5349b2d2478d3cf56f26a9e26a1e88
jesus9233 <jimyy9211@gmail.com>
2019-12-03 23:16:26 -0600

    final

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_REDIRECT_URL = '/'
+
 ALLOWED_HOSTS = ['127.0.0.1:8000', '.pythonanywhere.com']
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(template_name='registration/login.html'), name='logout'),
     path('', include('blog.urls')),
 ]
